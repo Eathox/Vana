dcdaf52dcc3b3b98a00a39329b6338175dcda3f9
Eathox <Rendier3636@gmail.com>
2018-01-12 15:15:01 +0100

    Update config_style_checker.py

--- tools/config_style_checker.py
+++ tools/config_style_checker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import fnmatch
 import os
 import re
@@ -7,20 +8,36 @@ import ntpath
 import sys
 import argparse
 
-def check_config_style(filepath):
+
+def validKeyWordAfterCode(content, index):
+    keyWords = ["for", "do", "count", "each", "forEach", "else", "and", "not", "isEqualTo", "in", "call", "spawn", "execVM", "catch", "param", "select", "apply"];
+    for word in keyWords:
+        try:
+            subWord = content.index(word, index, index+len(word))
+            return True;
+        except:
+            pass
+    return False
+
+
+def check_sqf_syntax(filepath):
     bad_count_file = 0
     def pushClosing(t):
         closingStack.append(closing.expr)
         closing << Literal( closingFor[t[0]] )
 
+
     def popClosing():
         closing << closingStack.pop()
+
 
     with open(filepath, 'r', encoding='utf-8', errors='ignore') as file:
         content = file.read()
 
+
         # Store all brackets we find in this file, so we can validate everything on the end
         brackets_list = []
+
 
         # To check if we are in a comment block
         isInCommentBlock = False
@@ -31,22 +48,29 @@ def check_config_style(filepath):
         # If the next character is a /, it means we end our comment block.
         checkIfNextIsClosingBlock = False
 
+
         # We ignore everything inside a string
         isInString = False
         # Used to store the starting type of a string, so we can match that to the end of a string
         inStringType = '';
 
+
         lastIsCurlyBrace = False
         checkForSemiColumn = False
 
+
         # Extra information so we know what line we find errors at
-        lineNumber = 0
+        lineNumber = 1
+
 
         indexOfCharacter = 0
         # Parse all characters in the content of this file to search for potential errors
         for c in content:
             if (lastIsCurlyBrace):
                 lastIsCurlyBrace = False
+                checkForSemiColumn = True
+
+
             if c == '\n': # Keeping track of our line numbers
                 lineNumber += 1 # so we can print accurate line number information when we detect a possible error
             if (isInString): # while we are in a string, we can ignore everything else, except the end of the string
@@ -54,6 +78,7 @@ def check_config_style(filepath):
                     isInString = False
             # if we are not in a comment block, we will check if we are at the start of one or count the () {} and []
             elif (isInCommentBlock == False):
+
 
                 # This means we have encountered a /, so we are now checking if this is an inline comment or a comment block
                 if (checkIfInComment):
@@ -63,6 +88,7 @@ def check_config_style(filepath):
                     elif (c == '/'): # Otherwise, will check if we are in an line comment
                         ignoreTillEndOfLine = True # and an line comment is a / followed by another / (//) We won't care about anything that comes after it
 
+
                 if (isInCommentBlock == False):
                     if (ignoreTillEndOfLine): # we are in a line comment, just continue going through the characters until we find an end of line
                         if (c == '\n'):
@@ -71,19 +97,21 @@ def check_config_style(filepath):
                         if (c == '"' or c == "'"):
                             isInString = True
                             inStringType = c
+                        elif (c == '#'):
+                            ignoreTillEndOfLine = True
                         elif (c == '/'):
                             checkIfInComment = True
                         elif (c == '('):
                             brackets_list.append('(')
                         elif (c == ')'):
-                            if (len(brackets_list) > 0 and brackets_list[-1] in ['{', '[']):
+                            if (brackets_list[-1] in ['{', '[']):
                                 print("ERROR: Possible missing round bracket ')' detected at {0} Line number: {1}".format(filepath,lineNumber))
                                 bad_count_file += 1
                             brackets_list.append(')')
                         elif (c == '['):
                             brackets_list.append('[')
                         elif (c == ']'):
-                            if (len(brackets_list) > 0 and brackets_list[-1] in ['{', '(']):
+                            if (brackets_list[-1] in ['{', '(']):
                                 print("ERROR: Possible missing square bracket ']' detected at {0} Line number: {1}".format(filepath,lineNumber))
                                 bad_count_file += 1
                             brackets_list.append(']')
@@ -91,13 +119,22 @@ def check_config_style(filepath):
                             brackets_list.append('{')
                         elif (c == '}'):
                             lastIsCurlyBrace = True
-                            if (len(brackets_list) > 0 and brackets_list[-1] in ['(', '[']):
+                            if (brackets_list[-1] in ['(', '[']):
                                 print("ERROR: Possible missing curly brace '}}' detected at {0} Line number: {1}".format(filepath,lineNumber))
                                 bad_count_file += 1
                             brackets_list.append('}')
                         elif (c== '\t'):
                             print("ERROR: Tab detected at {0} Line number: {1}".format(filepath,lineNumber))
                             bad_count_file += 1
+
+
+                        if (checkForSemiColumn):
+                            if (c not in [' ', '\t', '\n', '/']): # keep reading until no white space or comments
+                                checkForSemiColumn = False
+                                if (c not in [']', ')', '}', ';', ',', '&', '!', '|', '='] and not validKeyWordAfterCode(content, indexOfCharacter)): # , 'f', 'd', 'c', 'e', 'a', 'n', 'i']):
+                                    print("ERROR: Possible missing semi-column ';' detected at {0} Line number: {1}".format(filepath,lineNumber))
+                                    bad_count_file += 1
+
 
             else: # Look for the end of our comment block
                 if (c == '*'):
@@ -108,6 +145,7 @@ def check_config_style(filepath):
                     elif (c != '*'):
                         checkIfNextIsClosingBlock = False
             indexOfCharacter += 1
+
 
         if brackets_list.count('[') != brackets_list.count(']'):
             print("ERROR: A possible missing square bracket [ or ] in file {0} [ = {1} ] = {2}".format(filepath,brackets_list.count('['),brackets_list.count(']')))
@@ -120,38 +158,48 @@ def check_config_style(filepath):
             bad_count_file += 1
     return bad_count_file
 
+
 def main():
 
-    print("Validating Config Style")
+
+    print("Validating SQF")
+
 
     sqf_list = []
     bad_count = 0
+
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-m','--module', help='only search specified module addon folder', required=False, default="")
     args = parser.parse_args()
 
+
     # Allow running from root directory as well as from inside the tools directory
-    rootDir = "../Framework"
-    if (os.path.exists("Framework")):
-        rootDir = "Framework"
+    rootDir = "../addons"
+    if (os.path.exists("addons")):
+        rootDir = "addons"
+
 
     for root, dirnames, filenames in os.walk(rootDir + '/' + args.module):
-      for filename in fnmatch.filter(filenames, '*.cpp'):
-        sqf_list.append(os.path.join(root, filename))
-      for filename in fnmatch.filter(filenames, '*.hpp'):
+      for filename in fnmatch.filter(filenames, '*.sqf'):
         sqf_list.append(os.path.join(root, filename))
 
+
     for filename in sqf_list:
-        bad_count = bad_count + check_config_style(filename)
+        bad_count = bad_count + check_sqf_syntax(filename)
+
+
+
 
     print("------\nChecked {0} files\nErrors detected: {1}".format(len(sqf_list), bad_count))
     if (bad_count == 0):
-        print("Config validation PASSED")
+        print("SQF validation PASSED")
     else:
-        print("Config validation FAILED")
+        print("SQF validation FAILED")
+
 
     return bad_count
+
 
 if __name__ == "__main__":
     sys.exit(main())
