bfc643f0887505bd3556ee6c9ba291b0a988ce0a
Eathox <Rendier3636@gmail.com>
2018-01-16 03:03:46 +0100

    Update sqf_validator.py

--- tools/sqf_validator.py
+++ tools/sqf_validator.py
@@ -123,9 +123,9 @@ def check_sqf_syntax(filepath):
                                 print("ERROR: Possible missing curly brace '}}' detected at {0} Line number: {1}".format(filepath,lineNumber))
                                 bad_count_file += 1
                             brackets_list.append('}')
-                        elif (c== '\t'):
-                            print("ERROR: Tab detected at {0} Line number: {1}".format(filepath,lineNumber))
-                            bad_count_file += 1
+                        #elif (c== '\t'):
+                            #print("ERROR: Tab detected at {0} Line number: {1}".format(filepath,lineNumber))
+                            #bad_count_file += 1
 
 
                         if (checkForSemiColumn):
